3b4c86a8cdb78be89bf4109a2ba4f185b7f178ff
captainkong <kong-peng@qq.com>
2022-07-28 16:16:19 +0800

    echo 服务器的lfd读事件也交给线程池处理

--- code/poller/test.cpp
+++ code/poller/test.cpp
@@ -21,7 +21,7 @@ struct conn_t
 {
     int port;
     char ip[16];
-    Buffer readBuffer, writeBuffer;
+    Buffer readBuffer;
     conn_t(int _port, const char *_ip) : port(_port)
     {
         strncpy(ip, _ip, 16);
@@ -31,54 +31,14 @@ struct conn_t
 EPoller gEpoll_(MAX_USER);
 unordered_map<int, conn_t *> gMap;
 
-void readFromClient(conn_t *client, int index)
-{
-
-    cout << "readFromClient 线程id:" << this_thread::get_id() << endl;
-    // 处理客户端消息
-    int clientFd = gEpoll_.getFd(index);
-    int err;
-    while (true)
-    {
-        int n = client->readBuffer.readFd(clientFd, &err);
-        cout << "n=" << n << ", err=" << err << ", readable=" << client->readBuffer.readableBytes() << endl;
-        if (n < 1)
-        {
-            if (err == EAGAIN)
-            {
-                break;
-            }
-            // 出现错误或者对方主动关闭 下树
-            conn_t *tem = gMap[clientFd];
-            printf("Closed: %s:%d\n", tem->ip, tem->port);
-            delete tem;
-            gMap.erase(clientFd);
-            close(clientFd);
-            gEpoll_.delFd(clientFd);
-            return;
-        }
-    }
-
-    string str = client->readBuffer.retrieveAllToString();
-    for (int j = 0; j < static_cast<int>(str.size()); ++j)
-    {
-        if (str[j] >= 'a' && str[j] <= 'z')
-        {
-            str[j] -= 'a' - 'A';
-        }
-    }
-    send(clientFd, str.data(), str.size(), 0);
-    if (str[str.size() - 1] == '\n')
-        str[str.size() - 1] = '\0';
-    cout << "服务端转发:" << client->ip << ":" << client->port << " len=" << str.size() << endl;
-}
+void acceptNewClient(int lfd);
+void readFromClient(conn_t *client, int index);
 
 // 实现一个echo服务器
 int main(int argc, char const *argv[])
 {
-    int lfd, cfd, ret;
+    int lfd, ret;
     struct sockaddr_in addr;
-    // char buffer[1024];
     ThreadPool pool(8);
 
     // 创建套接字
@@ -99,42 +59,21 @@ int main(int argc, char const *argv[])
 
     // 监听
     ret = listen(lfd, MAX_USER);
-    gEpoll_.addFd(lfd, EPOLLIN);
+    gEpoll_.addFd(lfd, EPOLLIN | EPOLLET);
 
     while (1)
     {
         int ready = gEpoll_.wait();
-        // cout << "read:" << read << endl;
         for (int i = 0; i < ready; ++i)
         {
             if (gEpoll_.getFd(i) == lfd && gEpoll_.getEvent(i) & EPOLLIN)
             {
-                // 新的连接到来
-                struct sockaddr_in cliAddr;
-                socklen_t len = sizeof(cliAddr);
-                cfd = accept(lfd, (struct sockaddr *)&cliAddr, &len);
-                if (cfd == -1)
-                {
-                    perror("连接建立失败!");
-                }
-
-                // 将cfd设置为非阻塞模式
-                int flags = fcntl(cfd, F_GETFL);
-                flags |= O_NONBLOCK;
-                fcntl(cfd, F_SETFL, flags);
-
-                char ip[16];
-                inet_ntop(AF_INET, &cliAddr.sin_addr, ip, sizeof(ip));
-                conn_t *newConn = new conn_t(ntohs(cliAddr.sin_port), ip);
-                gMap[cfd] = newConn;
-                printf("New Connect: %s:%d\n", newConn->ip, newConn->port);
-                // 将新的连接添加到红黑树中
-                ret = gEpoll_.addFd(cfd, EPOLLIN | EPOLLET);
-                assert(ret);
+                // 新的连接到来  任务交给线程池处理
+                pool.addTask(bind(acceptNewClient, lfd));
             }
             else if (gEpoll_.getFd(i) != lfd && gEpoll_.getEvent(i) & EPOLLIN)
             {
-                // 处理客户端消息
+                // 处理客户端消息 任务交给线程池处理
                 int clientFd = gEpoll_.getFd(i);
                 pool.addTask(bind(readFromClient, gMap[clientFd], i));
             }
@@ -142,4 +81,73 @@ int main(int argc, char const *argv[])
     }
 
     return 0;
+}
+
+void readFromClient(conn_t *client, int index)
+{
+
+    // cout << "readFromClient 线程id:" << this_thread::get_id() << endl;
+    // 处理客户端消息
+    int clientFd = gEpoll_.getFd(index);
+    int err = 0;
+    while (true)
+    {
+        int n = client->readBuffer.readFd(clientFd, &err);
+        cout << "n=" << n << ", err=" << err << ", readable=" << client->readBuffer.readableBytes() << endl;
+        if (n < 1)
+        {
+            if (err == EAGAIN)
+            {
+                break;
+            }
+            // 出现错误或者对方主动关闭 下树
+            conn_t *tem = gMap[clientFd];
+            cout << "Closed: " << tem->ip << ":" << tem->port;
+            delete tem;
+            gMap.erase(clientFd);
+            close(clientFd);
+            gEpoll_.delFd(clientFd);
+            cout << " 剩余客户端:" << gMap.size() << endl;
+            return;
+        }
+    }
+
+    string str = client->readBuffer.retrieveAllToString();
+    for (int j = 0; j < static_cast<int>(str.size()); ++j)
+    {
+        if (str[j] >= 'a' && str[j] <= 'z')
+        {
+            str[j] -= 'a' - 'A';
+        }
+    }
+    send(clientFd, str.data(), str.size(), 0);
+    if (str[str.size() - 1] == '\n')
+        str[str.size() - 1] = '\0';
+    cout << "服务端转发:" << client->ip << ":" << client->port << " len=" << str.size() << endl;
+}
+
+void acceptNewClient(int lfd)
+{
+    // cout << "acceptNewClient 线程id:" << this_thread::get_id() << endl;
+    struct sockaddr_in cliAddr;
+    socklen_t len = sizeof(cliAddr);
+    int cfd = accept(lfd, (struct sockaddr *)&cliAddr, &len);
+    if (cfd == -1)
+    {
+        perror("连接建立失败!");
+    }
+
+    // 将cfd设置为非阻塞模式
+    int flags = fcntl(cfd, F_GETFL);
+    flags |= O_NONBLOCK;
+    fcntl(cfd, F_SETFL, flags);
+
+    char ip[16];
+    inet_ntop(AF_INET, &cliAddr.sin_addr, ip, sizeof(ip));
+    conn_t *newConn = new conn_t(ntohs(cliAddr.sin_port), ip);
+    gMap[cfd] = newConn;
+    printf("New Connect: %s:%d\n", newConn->ip, newConn->port);
+    // 将新的连接添加到红黑树中
+    int ret = gEpoll_.addFd(cfd, EPOLLIN | EPOLLET);
+    assert(ret);
 }
